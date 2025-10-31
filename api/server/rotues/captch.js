0cb4df017d51d3a119e605974ba1d74b5bf66b5d
midrra <el.mga.su@gmail.com>
2025-10-31 12:13:04 +0300

    merge cahnge

--- api/server/rotues/captch.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import {verifyCaptcha} from "../controllers/captchaController.js"
-
-// POST /api/captcha
-router.post("/", verifyCaptcha);
-
-export default router;
