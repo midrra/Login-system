dcd545c1094a43540a47f450a2abc83473abbbff
midrra <el.mga.su@gmail.com>
2025-10-31 12:18:06 +0300

    merge this two

---
+++ server/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import authRoutes from "./rotues/authRoutes.js";
+import homeRotues from "./rotues/homeRoutes.js";
+import captch from "./rotues/captch.js"
+import serverless from "serverless-http";
+
+
+const app = express();
+
+dotenv.config();
+connectDB();
+
+app.use(
+  cors({
+    origin: "https://login-system-chi-ruby.vercel.app",
+    credentials: true, // allow cookies / auth headers
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+app.use("/auth", authRoutes);
+app.use("/home", homeRotues);
+app.use("/api/captch",captch)
+
+app.get("/", (req, res) => {
+  res.send("✅ Backend running successfully!");
+});
+
+// export const handler = serverless(app);
+export default app;
