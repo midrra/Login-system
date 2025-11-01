9016cc409c5bc5ba0d0ccf310230f379cea4d0bb
midrra <el.mga.su@gmail.com>
2025-11-01 08:57:56 +0300

    add missing api call

--- client/src/components/GoogleLogin.jsx
+++ client/src/components/GoogleLogin.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { googleLogin } from "../api/auth";
+import api from "../api/axios";
 
 export function GoogleLogin() {
   const navigate = useNavigate();
